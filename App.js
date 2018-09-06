f163a003bd7a79d8d95a7bcd0f766a4a2158aafe
Jeff <jeffersonleon12@gmail.com>
2018-09-06 18:27:54 -0500

    as

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {StackNavigator} from 'react-navigation'
+import MainsScreen from './components/MainScreen'
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-       <Text>Hola mundo</Text>
-      </View>
+      <AppStackNavigator/>
     );
   }
 }
 
+const AppStackNavigator = createStackNavigator({
+  Main:{
+    screen: MainsScreen
+  }
+})
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ components/MainScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class MainScreen extends React.Component {
+  render() {
+    return (
+        <Text>Hola</Text>
+        );
+  }
+}
